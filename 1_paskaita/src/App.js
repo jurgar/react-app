dcc140946c160d3d22ecc25c79b4734c80da414a
jurgar <102683510+jurgar@users.noreply.github.com>
2022-09-25 20:41:31 +0300

    add React examples

---
+++ 1_paskaita/src/App.js
@@ -0,0 +1,18 @@
+import "./App.css";
+
+// JSX  struktura kuri renderina JAVASCRIPT +HTML
+
+function App() {
+  return (
+    <div>
+      <header>
+        <h1>Logo</h1>
+      </header>
+
+      <main></main>
+      <footer></footer>
+    </div>
+  );
+}
+
+export default App;
